6b76913cb01d14c731270b671f24efe619bcc6a4
AlienQbw <wladyka.bj@gmail.com>
2021-09-23 21:13:06 +0200

    slight eddit on project description (todo) in productivity app

--- src/sites/ProductivityApp.js
+++ src/sites/ProductivityApp.js
@@ -1,9 +1,41 @@
 import React from 'react';
 
+/*
+This app should contain:
+            -Timer for the single activity
+            -Todo list
+            -Personal Settings for breaks and work
+            -History for every session
+            -Background music for focus
+            -Categories for the tasks
+*/
+
 const ProductivityApp = () => {
   return (
     <div>
-      <p>productivity app site</p>
+      <p>
+        1. Timer for single activity with switch to go to studying/working timer
+        or break Timer
+      </p>
+      <p>2. Todo list</p>
+      <p>
+        3. Settings button and settings will contain:
+        <ul>
+          <li style={{ listStyle: 'inside', marginLeft: '5px' }}>
+            Timer settings
+          </li>
+          <li style={{ listStyle: 'inside', marginLeft: '5px' }}>
+            Background music genre
+          </li>
+        </ul>
+      </p>
+      <p>4. History of previous sessions</p>
+      <p>5. Background music play button and volume slider</p>
+      <p>
+        6. SIDEBAR which will slide thanks to hamburger button, and will contain
+        categories and tasks, which will have buttons to add them to main screed
+        todo list
+      </p>
     </div>
   );
 };

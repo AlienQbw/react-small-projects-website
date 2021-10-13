0e04b7dd2c6c73de77ecfcfc2c6cf2cf33f129cb
AlienQbw <wladyka.bj@gmail.com>
2021-10-13 06:38:55 +0200

    styled music component

--- src/modules/ProductivityAppModules/Music.js
+++ src/modules/ProductivityAppModules/Music.js
@@ -4,16 +4,21 @@ import { useGlobalProductivityContext } from '../../productivityContext';
 const Music = () => {
   const { musicGenre } = useGlobalProductivityContext();
   return (
-    <div className="productivity-module container">
+    <div className="productivity-container music-container">
       <p>
         Choose the music that you would like to be played while you are working
         on something ;)
       </p>
-
-      {musicGenre.map((el) => {
-        return <button key={el}>{el}</button>;
-      })}
-      <button>No music</button>
+      <div className="music-options-container flex">
+        {musicGenre.map((el) => {
+          return (
+            <button className="btn-productivity" key={el}>
+              {el}
+            </button>
+          );
+        })}
+        <button className="btn-productivity">No music</button>
+      </div>
     </div>
   );
 };

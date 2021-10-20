7ea7a6d5eb0445f0c6a78aa49843ba6f62f1bf26
AlienQbw <wladyka.bj@gmail.com>
2021-10-20 23:00:48 +0200

    added functionality to music component, now what is left is to add functionality to different music buttons, so that we can change song

--- src/modules/ProductivityAppModules/Music.js
+++ src/modules/ProductivityAppModules/Music.js
@@ -1,8 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useGlobalProductivityContext } from '../../productivityContext';
+import lofi from './media/music/lofi-sample.mp3';
+import rock from './media/music/rock-sample.mp3';
+import instrumental from './media/music/instrumental-sample.mp3';
+import pop from './media/music/pop-sample.mp3';
+/* I'm importing in such a very inefficient way because I have only 4 songs */
+
+const useAudio = (url) => {
+  const [audio] = useState(new Audio(url));
+  const [playing, setPlaying] = useState(false);
+  const volume = useRef(1);
+
+  const toggle = () => {
+    setPlaying(!playing);
+  };
+  const changeVolume = () => {
+    audio.volume = volume.current.value;
+  };
+
+  useEffect(() => {
+    playing ? audio.play() : audio.pause();
+  }, [playing]);
+
+  useEffect(() => {
+    audio.addEventListener('ended', () => setPlaying(false));
+    return () => {
+      audio.removeEventListener('ended', () => setPlaying(false));
+    };
+  }, []);
+
+  return [playing, toggle, volume, changeVolume];
+};
 
 const Music = () => {
   const { musicGenre } = useGlobalProductivityContext();
+  const url = lofi;
+
+  const [playing, toggle, volume, changeVolume] = useAudio(url);
   return (
     <div className="productivity-container music-container">
       <p>
@@ -12,14 +46,32 @@ const Music = () => {
       <div className="music-options-container flex">
         {musicGenre.map((el) => {
           return (
-            <button className="btn-productivity" key={el}>
+            <button
+              className="btn-productivity"
+              key={el}
+              onClick={() => console.log(`change song!`)}
+            >
               {el}
             </button>
           );
         })}
-        <button className="btn-productivity">No music</button>
-        <label>Volume:</label>
-        <input type="range" id="volume" name="volume" min="0" max="20" />
+        <div className="player-button">
+          <button
+            className={playing ? 'btn-pause' : 'btn-play'}
+            onClick={toggle}
+          ></button>
+        </div>
+        <input
+          onClick={changeVolume}
+          ref={volume}
+          type="range"
+          id="volume"
+          name="volume"
+          min="0"
+          max="1"
+          step="0.02"
+        />
+        <label>100%</label>
       </div>
     </div>
   );
